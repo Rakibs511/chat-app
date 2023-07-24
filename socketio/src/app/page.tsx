a3fc0034ce3dde5e36780ae87ab3fbb6ae7a2902
Rakibul Hasan <rakibulhasan20162@gmail.com>
2023-07-24 07:56:55 +0600

    add scroll-to-bottom and fix some design make better design

--- socketio/src/app/page.tsx
+++ socketio/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useTransition } from "react";
+import { useEffect, useState, useTransition, useRef } from "react";
 import { io } from "socket.io-client";
 
 const socket = io("http://192.168.1.109:4000");
@@ -7,20 +7,25 @@ export default function Home() {
   const [msg, setMsg] = useState<string | null>(null);
   const [msgElement, setMsgElement] = useState<string[]>([]);
   const [isPending, startTransition] = useTransition();
+  const refScrollBottom = useRef<HTMLDivElement>(null);
 
-//   send msg with type delay
-//   useEffect(() => {
-//     const test = setTimeout(() => {
-//       // msg && setMsgElement((prev) => (prev = [...prev, msg]));
-//       msg && socket.emit("send_message", msg);
-//     }, 1000);
-//     return () => clearTimeout(test);
-//   }, [msg]);
+  //   send msg with type delay
+  //   useEffect(() => {
+  //     const test = setTimeout(() => {
+  //       // msg && setMsgElement((prev) => (prev = [...prev, msg]));
+  //       msg && socket.emit("send_message", msg);
+  //     }, 1000);
+  //     return () => clearTimeout(test);
+  //   }, [msg]);
+
+  useEffect(() => {
+    refScrollBottom?.current?.scrollIntoView();
+  }, [msgElement]);
 
   const sendMsgHandle = () => {
     try {
       socket.emit("send_message", msg);
-      setMsg('')
+      setMsg("");
     } catch (error: any) {
       console.log(error);
     }
@@ -29,34 +34,38 @@ export default function Home() {
     socket.on("received_message", (data) => {
       setMsgElement((prev) => (prev = [...prev, data]));
     });
+    return () => {
+      socket.off("received_message");
+    };
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="sticky top-0 backdrop-blur-md">
+    <main className="flex min-h-screen flex-col items-center p-24 scroll-smooth">
+      <div className="sticky top-0 min-w-[100vw] backdrop-blur-md flex flex-col items-center">
         <h1 className="text-5xl p-3">🟢 Live Chat</h1>
-        <div className="flex space-x-2">
-          <input
-            className="text-black  p-3  rounded-md border-r-4"
-            type="text"
-            value={msg?msg:''}
-            placeholder="Enter a message"
-            onChange={(e) => setMsg(e.target.value)}
-            onKeyDown={(e)=>e.key==="Enter"&&sendMsgHandle()}
-          />
-
-          <button
-            className="bg-slate-500 hover:bg-slate-400 p-3 rounded-md"
-            onClick={sendMsgHandle}
-          >
-            Send
-          </button>
-        </div>
       </div>
-      <div className="p-5">
+      <div className="flex space-x-2 fixed bottom-5">
+        <input
+          className="text-black  p-3  rounded-md border-r-4"
+          type="text"
+          value={msg ? msg : ""}
+          placeholder="Enter a message"
+          onChange={(e) => setMsg(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && sendMsgHandle()}
+        />
+
+        <button
+          className="bg-slate-500 hover:bg-slate-400 p-3 rounded-md"
+          onClick={sendMsgHandle}
+        >
+          Send
+        </button>
+      </div>
+      <div className="p-5 w-[500px] text-left">
         {msgElement?.map((msg, index) => {
           return <Message key={index} content={msg} />;
         })}
+        <div ref={refScrollBottom} />
       </div>
     </main>
   );
